ad3569406a97321a2f0b4c1a4f0b6f158f8877b6
Vendela Eklund <TOXIC_FROG_KARLSKRONA>
2024-02-02 23:07:38 +0100

    Fix stoopid white arrow...

---
+++ xml_formatter/src/App.js
@@ -0,0 +1,97 @@
+import { useState } from "react"
+
+const emptyTag = ""
+const PEOPLE_TAG = "people"
+const PERSON_TAG = "person"
+const TELEPHONE_TAG = "telephone"
+const ADDRESS_TAG = "address"
+const FAMILY_TAG = "family"
+
+function App() {
+    const [input, setInput] = useState("P | förnamn | efternamn \nT | mobilnummer | fastnätsnummer \nT | mobilnummer | fastnätsnummer \nA | gata | stad | postnummer \nF | namn | födelseår \nP | förnamn | efternamn \nA | gata | stad | postnummer ")
+    const [output, setOutput] = useState("OUTPUT")
+
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={(e) => {
+                    setInput(e.target.value)
+                }}
+                multiple
+                style={{ height: "400px", width: "400px" }}//={{ height: `${Math.sin(input.length / 30) * 200 + 500}px`, width: "800px", transform: `rotate(${input.length*10}deg)`, transition: "all 0.3s ease-out" }}
+            />
+            <button
+                onClick={() => formatButton(input, setOutput)}
+                style={{ height: "60px", width:"100px"}}
+            >
+                Format
+            </button>
+            <textarea
+                value={output}
+                multiple
+                style={{ height: "400px", width: "400px" }}
+            />
+        </div>
+    )
+}
+
+function formatButton(input, setOutput) {
+    fetch("http://localhost:8000/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+    fetch("http://localhost:8000/",
+        {
+            method: "POST",
+            body: JSON.stringify({ input:input }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(res => res.json())
+        .then(result => setOutput(result))
+}
+
+function formatInfoToXML(element, index) {
+    console.log(element.type)
+    let result = ""
+
+    switch (element.type) {
+        case "Person":
+            result += StartTag(PERSON_TAG)
+            result += EndTag(PERSON_TAG)
+            break
+        case "Telephone":
+            result += StartTag(TELEPHONE_TAG)
+            result += EndTag(TELEPHONE_TAG)
+            break
+        case "Address":
+            result += StartTag(ADDRESS_TAG)
+            result += EndTag(ADDRESS_TAG)
+            break
+        case "Family":
+            result += StartTag(FAMILY_TAG)
+            result += EndTag(FAMILY_TAG)
+            break
+        default:
+            console.log("[formatInfoToXML] UNDEFINED ELEMENT")
+            break
+    }
+    return result
+}
+
+function StartTag(tag) {
+    return `<${tag}>`
+}
+
+function EndTag(tag) {
+    return `</${tag}>`
+}
+
+function Indentation() {
+    return "    "
+}
+
+export default App;
---
+++ xml_formatter/src/server.js
@@ -0,0 +1,162 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const cors = require('cors');
+const app = express()
+const port = 8000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({
+        text: "HELLO SERVER!"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/', bodyParser.json(), (req, res) => {
+    console.log(req.body)
+    res.json(transformInputToPeople(req.body.input))
+})
+
+// ========= TYPES ==========
+
+
+class Telephone {
+    type = "Telephone";
+    constructor(telephoneLine) {
+        this.mobile = telephoneLine.length > 1 ? telephoneLine[1] : ""
+        this.landline = telephoneLine.length > 2 ? telephoneLine[2] : ""
+    }
+}
+
+class Address {
+    type = "Address";
+    constructor(addressLine) {
+        this.street = addressLine.length > 1 ? addressLine[1] : ""
+        this.city = addressLine.length > 2 ? addressLine[2] : ""
+        this.zipcode = addressLine.length > 3 ? addressLine[3] : ""
+    }
+}
+
+class Person {
+    type = "Person"
+
+    constructor(personLine) {
+        this.firstname = personLine.length > 1 ? personLine[1] : ""
+        this.surname = personLine.length > 2 ? personLine[2] : ""
+        this.includedInfoList = []
+    }
+
+    addTelephone(telephone) {
+        if (this.includedInfoList.some(e => e.type === "Telephone")) {
+            console.log("[Person.addTelephone] TELEPHONE ALREADY EXIST FOR THIS PERSON")
+            return
+        }
+        this.includedInfoList.push(telephone)
+    }
+
+    addAddress(address) {
+        if (this.includedInfoList.some(e => e.type === "Address")) {
+            console.log("[Person.addAddress] ADDRESS ALREADY EXIST FOR THIS PERSON")
+            return
+        }
+        this.includedInfoList.push(address)
+    }
+
+    addFamilyMember(familyMember) {
+        this.includedInfoList.push(familyMember)
+    }
+}
+
+class FamilyMember {
+    type = "FamilyMember"
+    constructor(familyLine) {
+        this.includedInfoList = []
+        this.name = familyLine.length > 1 ? familyLine[1] : ""
+        this.born = familyLine.length > 2 ? familyLine[2] : ""
+    }
+
+    addTelephone(telephone) {
+        if (this.includedInfoList.some(e => e.type === "Telephone")) {
+            console.log("[FamilyMember.addTelephone] TELEPHONE ALREADY EXIST FOR THIS PERSON")
+            return
+        }
+        this.includedInfoList.push(telephone)
+    }
+
+    addAddress(address) {
+        if (this.includedInfoList.some(e => e.type === "Address")) {
+            console.log("[FamilyMember.addAddress] ADDRESS ALREADY EXIST FOR THIS PERSON")
+            return
+        }
+        this.includedInfoList.push(address)
+    }
+}
+
+
+// ========= MY SHIT ==============
+function transformInputToPeople(input) {
+    let lines = getLines(input)
+    let arrayOfPeople = getPeopleArray(lines)
+    return JSON.stringify(arrayOfPeople)
+}
+
+function getLines(input) {
+    return input.split("\n")
+}
+
+function getPeopleArray(lines) {
+    let person = undefined
+    let familyMember = undefined
+    let arrayOfPeople = []
+
+    for (let i = 0, n = lines.length; i < n; i++) {
+        let line = lines[i]
+        let allWordsInLine = line.split('|')
+        switch (line.charAt(0)) {
+            case 'P':
+                if (familyMember) {
+                    person.addFamilyMember(familyMember)
+                    familyMember = undefined
+                }
+                if (person) {
+                    arrayOfPeople.push(person)
+                }
+                person = new Person(allWordsInLine)
+                continue
+            case 'T':
+                if (!person) continue
+                if (familyMember) {
+                    familyMember.addTelephone(new Telephone(allWordsInLine))
+                    continue
+                }
+                person.addTelephone(new Telephone(allWordsInLine))
+                continue
+            case 'A':
+                if (!person) continue
+                if (familyMember) {
+                    familyMember.addAddress(new Address(allWordsInLine))
+                    continue
+                }
+                person.addAddress(new Address(allWordsInLine))
+                continue
+            case 'F':
+                if (!person) continue
+                if (familyMember) {
+                    person.addFamilyMember(familyMember)
+                }
+                familyMember = new FamilyMember(allWordsInLine)
+                continue
+            default:
+                console.log("[newAdd] UNDEFINED CHARACTER")
+                break;
+        };
+    }
+    if (familyMember) { person.addElement(familyMember) }
+    if (person) { arrayOfPeople.push(person) }
+    return arrayOfPeople;
+}
+
